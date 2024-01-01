be912775e4b3cdfbbd23aaa5edab10bd804c3486
KacperOdzimek <kacper-odzimek@wp.pl>
2024-01-01 18:54:59 +0100

    Allow multiple color buffers in framebuffer

--- source/objects/framebuffer.h
+++ source/objects/framebuffer.h
@@ -13,32 +13,32 @@ namespace graphics_abstraction
 			struct framebuffer : public graphics_abstraction::framebuffer
 			{
 				internal::GLuint id = 0;
-				framebuffer() {};
-				framebuffer(graphics_abstraction::texture* _color_buffer, graphics_abstraction::texture* _depth_stencil_buffer, graphics_abstraction::api*& api)
+				framebuffer()
 				{					
 					internal::glGenFramebuffers(1, &id);
 					internal::glBindFramebuffer(GL_FRAMEBUFFER, id);
-
-					set_color_buffer(_color_buffer);
-					set_depth_stencil_buffer(_depth_stencil_buffer);
 				}
 
 			protected:
-				virtual void set_color_buffer_impl()
+				virtual void set_color_buffer_impl(uint8_t number)
 				{
-					switch (color_buffer->texture_type)
+					switch (color_buffers.at(number)->texture_type)
 					{
 					case graphics_abstraction::texture_type::texture_1d:
-						internal::glFramebufferTexture1D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_1D, static_cast<texture*>(color_buffer)->id, 0);
+						internal::glFramebufferTexture1D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, 
+							GL_TEXTURE_1D, static_cast<texture*>(color_buffers.at(number))->id, 0);
 						break;
 					case graphics_abstraction::texture_type::texture_2d:
-						internal::glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, static_cast<texture*>(color_buffer)->id, 0);
+						internal::glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, 
+							GL_TEXTURE_2D, static_cast<texture*>(color_buffers.at(number))->id, 0);
 						break;
 					case graphics_abstraction::texture_type::texture_3d:
-						internal::glFramebufferTexture3D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_3D, static_cast<texture*>(color_buffer)->id, 0, 0);
+						internal::glFramebufferTexture3D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, 
+							GL_TEXTURE_3D, static_cast<texture*>(color_buffers.at(number))->id, 0, 0);
 						break;
 					case graphics_abstraction::texture_type::renderbuffer:
-						internal::glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, static_cast<texture*>(color_buffer)->id);
+						internal::glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, 
+							GL_RENDERBUFFER, static_cast<texture*>(color_buffers.at(number))->id);
 						return;
 					}
 				}
@@ -74,7 +74,13 @@ namespace graphics_abstraction
 			{
 				virtual object* build_impl(graphics_abstraction::api*& api)
 				{
-					return new framebuffer{ color_buffer, depth_stencil_buffer, api };
+					auto fb = new framebuffer{};
+					for (auto& text : color_buffers)
+					{
+						fb->set_color_buffer(text.first, text.second);
+					}
+					fb->set_depth_stencil_buffer(depth_stencil_buffer);
+					return fb;
 				}
 			};
 		}
